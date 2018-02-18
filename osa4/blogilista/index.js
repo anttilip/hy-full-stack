062919ada92d2424d4464a9e2c8951c9332e67b9
Antti Lipsanen <antti.lip@gmail.com>
2018-02-18 12:19:50 -0700

    Add part 4 ex 1-2

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+
+const mongoUrl = 'mongodb://test:test@ds113738.mlab.com:13738/hy-full-stack'
+mongoose.connect(mongoUrl)
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.logger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.error)
+
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
