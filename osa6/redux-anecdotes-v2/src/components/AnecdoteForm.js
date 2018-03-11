6b6727ca137ae69ce962330522f5512308787ec9
anttilip <antti.lip@gmail.com>
2018-03-11 17:20:26 +0200

    Clean up

--- osa6/redux-anecdotes-v2/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes-v2/src/components/AnecdoteForm.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 
 class AnecdoteForm extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   handleSubmit(e) {
     e.preventDefault()
     const content = e.target.anecdote.value
